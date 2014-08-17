05154c2923db7606c4ef01301143b95e868590c2
Gerald Leenerts <gerald.leenerts@gmail.com>
2014-08-16 17:11:57 -0700

    port and IP setup...

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.set('ipaddr', ipaddress);
+app.set('port', port);
+
 app.engine('html', ejs.renderFile);
 app.use(express.static(process.cwd() + '/public'));
-
 
 app.get('/', function(req, res){
   res.render('index.html');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(app.get('port'), app.get('ipaddr'), function() {
   console.log('Listening on port %d', server.address().port);
 });
